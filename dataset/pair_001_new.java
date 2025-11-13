e1c9e25dcdb4658ce5890391157db17cb126fcdb
EmmanuelOmonua <omonua@uwindsor.ca>
2025-11-12 20:00:22 -0500

    Update pair_001_new.java

--- dataset/pair_001_new.java
+++ dataset/pair_001_new.java
@@ -1,3 +1,7 @@
-System.out.println("Hello");
-int number = 5;
-System.out.println("Good bye");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int number = 5;
+        System.out.println("Good bye!");
+    }
+}
